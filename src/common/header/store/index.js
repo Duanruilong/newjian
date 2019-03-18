b62f7557e9cc400907514338444739411ef1982e
drlong <drl1210@163.com>
2019-03-19 00:33:42 +0800

    combineReducers  合并拆解的reducer文件，拆分路径

---
+++ src/common/header/store/index.js
@@ -0,0 +1,4 @@
+// 重新定义下路径
+import reducer from './reducer';
+
+export {reducer};
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,11 @@
-const defaultState = {
-    focused:false,
-};
 
-export default (state = defaultState, action)=>{
-    if (action.type === 'search_focus') {
-        return {
-            focused:true
-        };
-    }
-    if (action.type === 'search_blur') {
-        return {
-            focused:false
-        };
-    }
-    return state;
-}
+
+// combineReducers 合并小的reduce
+import { combineReducers } from 'redux';
+import {reducer as headerReducer } from '../common/header/store'
+
+const reducer = combineReducers({
+    header:headerReducer,
+})
+
+export default reducer;
