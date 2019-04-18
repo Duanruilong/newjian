0dbe7e9605da79e8112c71272655eec4babc7b96
drlong <drl1210@163.com>
2019-04-19 00:38:11 +0800

    标签换一换动画

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -32,8 +32,11 @@ class Header extends Component {
             <SearchInfoTitle>
               热门搜索
               <SearchInfoSwitch 
-                onClick={()=>handleChangePage(page,totalPage,this.spinIcon)}
-              >换一批</SearchInfoSwitch>
+                onClick={()=>handleChangePage(page,totalPage,this.spinIcon)}>
+                <i ref={(icon)=>{this.spinIcon = icon}} className="iconfont spin">&#xe637;</i>
+
+                换一批
+              </SearchInfoSwitch>
             </SearchInfoTitle>
             <SearchInfoList>
               {pageList}
@@ -118,7 +121,15 @@ const mapDispathToProps = (dispatch, ownProps) => {
     handleMouseLeave(){ // 鼠标移入事件
       dispatch(actionCreators.mouseLeave());
     },
-    handleChangePage(page,totalPage){
+    handleChangePage(page,totalPage, spin){
+      // 搜索旋转动画
+      let originAngle = spin.style.transform.replace(/[^0-9]/ig, '');
+      if (originAngle) {
+        originAngle = parseInt(originAngle,10)
+      } else {
+        originAngle = 0
+      }
+      spin.style.transform = 'rotate('+(originAngle+360)+'deg)'
       if (page < totalPage) {
         dispatch(actionCreators.changePage(page + 1));
       } else {
